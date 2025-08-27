7d56e2dc2b1e6199814c41e1a267e8f181df25a9
Jing I Wu <jingiwu@Eva-Mac-mini.local>
2025-08-27 20:10:08 +0800

    chore(release): add @semantic-release/git to commit CHANGELOG and version bump

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -3,6 +3,7 @@ export default  {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    "@semantic-release/git",
     '@semantic-release/changelog',
     '@semantic-release/github',
   ],
